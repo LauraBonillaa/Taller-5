315b4c396d6e90b643314206425fffde96c1ebd7
LauraBonillaa <158177899+LauraBonillaa@users.noreply.github.com>
2024-03-06 09:31:19 -0500

    Taller 5

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = prompt("¿Cual es su nombre?");
+let LastName = prompt("¿Cual es su apellido?");
+let AñoNacimiento = prompt("¿En que año naciste?");
+let DiaNacimiento = prompt("¿En que día naciste?");
+let MesNacimiento = prompt("¿En que mes naciste?");
+const AñoNacimientoo = parseInt (AñoNacimiento)
+const edad = (2024-AñoNacimientoo)
+
+console.log ("Mi nombre es ",name," ",LastName,", mi edad es ", edad, "y nací el ", DiaNacimiento, "de ", MesNacimiento," de ", AñoNacimiento)
